ff70922d7476049f73b0493f42a8e379af91bbf2
MathieuDacheux <mathieu.dacheux@icloud.com>
2022-09-27 11:33:41 +0200

    Push

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -2,8 +2,21 @@
 
 // Variables
 
+
+
 // Appel des configurations
+
+
 
 // Appel des fonctions
 
+
+
 // Appel des vues
+
+include(__DIR__.'/../views/templates/header.php');
+
+include(__DIR__.'/../views/home.php');
+
+include(__DIR__.'/../views/templates/footer.php');
+
--- views/templates/header.php
+++ views/templates/header.php
@@ -5,7 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" href="./public/css/style.css">
-    <title>Nouveau projet</title>
+    <link rel="stylesheet" href="/public/css/style.css">
+    <link href="https://api.fontshare.com/v2/css?f[]=general-sans@500,600,400,700&display=swap" rel="stylesheet">
+    <link href="https://api.fontshare.com/v2/css?f[]=zodiak@400&display=swap" rel="stylesheet">
+    <title>RSS Reader</title>
 </head>
 <body>
+    <header>
+    
+    </header>
