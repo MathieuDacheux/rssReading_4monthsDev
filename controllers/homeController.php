62454235f133b6c5ed044e62560d692c63ae5712
MurielSauty <107468021+MurielSauty@users.noreply.github.com>
2022-09-27 15:33:03 +0200

    Header et nav + card

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -1,9 +1,10 @@
 <?php
 
 // Variables
-
+$title='Entreprendre';
 // Appel des configurations
-
+require('../config/config.php');
 // Appel des fonctions
 
 // Appel des vues
+include('../views/templates/header.php');
--- views/templates/header.php
+++ views/templates/header.php
@@ -1,11 +1,87 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" href="./public/css/style.css">
+    <link rel="stylesheet" href="../public/css/style.css">
     <title>Nouveau projet</title>
 </head>
+
 <body>
+    <!-- En tête -->
+    <header>
+        <!-- Menu navigation -->
+        <nav class="navbar navbar-expand-lg navbar-light p-4 mb-4 ">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="..\public\img\logo_Flux-RSS.png" alt="Logo" width="200px" class="d-inline-block align-text-center">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Acceuil</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Entreprises</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Argent</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Economie Française</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Immobilier</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Emploi</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- Contenu -->
+    <main>
+        <section class="d-flex flex-column justify-content-center m-auto">
+            <!-- Card 1 -->
+            <div class="card m-auto mb-5 " style="width: 20rem;">
+
+                <div class="card-body d-flex m-auto p-auto">
+                    <img src="..." class="card-img m-5" alt="...">
+                    
+                    <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                </div>
+            </div>
+            <!-- Card 2 -->
+            <div class="card m-auto mb-5" style="width: 20rem;">
+
+                <div class="card-body d-flex m-auto p-auto">
+                    <img src="..." class="card-img m-5" alt="...">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <!-- Card 3 -->
+            <div class="card m-auto mb-5" style="width: 20rem;">
+
+                <div class="card-body d-flex m-auto p-auto">
+                    <img src="..." class="card-img m-5" alt="...">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <!-- Card 4 -->
+            <div class="card m-auto mb-5" style="width: 20rem;">
+
+                <div class="card-body d-flex m-auto p-auto">
+                    <img src="..." class="card-img m-5" alt="...">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <!-- Card 5 -->
+            <div class="card m-auto mb-5" style="width: 20rem;">
+
+                <div class="card-body d-flex m-auto p-auto">
+                    <img src="..." class="card-img m-5" alt="...">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </section>
+
+
+    </main>
