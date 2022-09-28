e048beadcc12a85d43d107319f11f9871ee4b06e
AlexisGaillet <alexisgaillet36@gmail.com>
2022-09-28 11:31:36 +0200

    ajout du footer

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -9,11 +9,11 @@
             <div class="modal-body">
                 <div class="containerCheckbox">
                     <ul class="ks-cboxtags">
-                        <li><input type="checkbox" id="checkboxOne" value="Rainbow Dash"><label for="checkboxOne">Entreprises</label></li>
-                        <li><input type="checkbox" id="checkboxTwo" value="Cotton Candy"><label for="checkboxTwo">Argent</label></li>
-                        <li><input type="checkbox" id="checkboxThree" value="Rarity"><label for="checkboxThree">Economie Française</label></li>
-                        <li><input type="checkbox" id="checkboxFour" value="Moondancer"><label for="checkboxFour">Immobilier</label></li>
-                        <li><input type="checkbox" id="checkboxFive" value="Surprise"><label for="checkboxFive">Emploi</label></li>
+                        <li><input type="checkbox" id="checkboxOne" value="Entreprises"><label for="checkboxOne">Entreprises</label></li>
+                        <li><input type="checkbox" id="checkboxTwo" value="Argent"><label for="checkboxTwo">Argent</label></li>
+                        <li><input type="checkbox" id="checkboxThree" value="Economie Française"><label for="checkboxThree">Economie Française</label></li>
+                        <li><input type="checkbox" id="checkboxFour" value="Immobilier"><label for="checkboxFour">Immobilier</label></li>
+                        <li><input type="checkbox" id="checkboxFive" value="Emploi"><label for="checkboxFive">Emploi</label></li>
                     </ul>
                 </div>
             </div>
@@ -24,6 +24,17 @@
         </div>
     </div>
 </div>
+<footer class="text-center">
+    <section class="d-flex flex-sm-row flex-column justify-content-center justify-content-sm-arround align-items-center m-auto">
+        <div class="contact">
+            <h6>Contact</h6>
+            <a href="mailto:contact.entreprendre@gmail.com" title="Cliquez sur le mail!" id="contactMail">contact.entreprendre@gmail.com</a>
+        </div>
+        <div class="foot">
+            <p>Copyright© 2022</p>
+        </div>
+    </section>
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
 
